e5510af9478974a16b64b870f49a0de8beb2446a
Leo TIOLLIER <ltiollier30@gmail.com>
2022-11-24 10:22:55 +0100

    :sparkles: Don't route when not in allowedEnv

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,22 @@ use Webid\Ail\Http\Controllers\ImpersonateController;
 use Webid\Ail\Http\Middleware\CanImpersonate;
 use Webid\Ail\Http\Middleware\IsImpersonating;
 
-Route::prefix(config('ail.routes.prefix'))
-    ->middleware(config('ail.routes.middlewares', []))
-    ->name(config('ail.routes.name').'.')
-    ->group(function () {
-        Route::get('impersonate/take/{id}/{guardName?}', [ImpersonateController::class, 'take'])
-            ->name('impersonate')
-            ->middleware([
-                CanImpersonate::class,
-            ]);
-        Route::get('impersonate/leave', [ImpersonateController::class, 'leave'])
-            ->name('impersonate.leave')
-            ->middleware([
-                IsImpersonating::class,
-            ]);
-        Route::get('{guard?}', [AilController::class, 'index'])
-            ->name('index');
-    });
+if (in_array(config('app.env'), config('ail.allowedEnv'))) {
+    Route::prefix(config('ail.routes.prefix'))
+        ->middleware(config('ail.routes.middlewares', []))
+        ->name(config('ail.routes.name').'.')
+        ->group(function () {
+            Route::get('impersonate/take/{id}/{guardName?}', [ImpersonateController::class, 'take'])
+                ->name('impersonate')
+                ->middleware([
+                    CanImpersonate::class,
+                ]);
+            Route::get('impersonate/leave', [ImpersonateController::class, 'leave'])
+                ->name('impersonate.leave')
+                ->middleware([
+                    IsImpersonating::class,
+                ]);
+            Route::get('{guard?}', [AilController::class, 'index'])
+                ->name('index');
+        });
+}
